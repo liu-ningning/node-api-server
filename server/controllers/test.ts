b3278d90fc31c556d7fdd263f54c401b0f26e578
liuningning <liuningning@blued.com>
2022-11-15 21:26:28 +0800

    feat: Initial commint

--- server/controllers/test.ts
+++ server/controllers/test.ts
@@ -4,12 +4,14 @@ import moment from 'moment'
 export default {
   test: async (ctx: Context) => {
     const { uid, name } = ctx
+
     ctx.send({
       data: {
         uid,
         name,
-        id_name: 'node-api',
+        id_name: 'node-api-server',
         timestamp: moment().format('YYYY-MM-DD HH:mm:ss'),
+        NODE_ENV: process.env.NODE_ENV
       },
     })
   },
